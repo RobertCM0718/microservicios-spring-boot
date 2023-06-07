627c4014512c2d51fd790ba42f44e65a94c4a32a
RobertCM0718 <robertcm0718@gmail.com>
2023-06-07 17:51:41 -0600

    Se agrega implementación de cliente rest Feign de netflix.

--- microservicio-item/src/main/java/com/quetzalcode/items/controller/ItemController.java
+++ microservicio-item/src/main/java/com/quetzalcode/items/controller/ItemController.java
@@ -3,6 +3,7 @@ package com.quetzalcode.items.controller;
 import com.quetzalcode.items.dto.Item;
 import com.quetzalcode.items.service.IItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 public class ItemController {
 
     @Autowired
+    @Qualifier("serviceFeign")
     private IItemService iItemService;
 
     @GetMapping("/listar")
---
+++ microservicio-item/src/main/java/com/quetzalcode/items/service/ItemServiceFeign.java
@@ -0,0 +1,28 @@
+package com.quetzalcode.items.service;
+
+import com.quetzalcode.items.cliente.ProductoClienteRest;
+import com.quetzalcode.items.dto.Item;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service("serviceFeign")
+@Primary
+public class ItemServiceFeign implements IItemService{
+
+    @Autowired
+    private ProductoClienteRest productoClienteRest;
+
+    @Override
+    public List<Item> findAll() {
+        return productoClienteRest.listar().stream().map(producto -> new Item(producto,1)).collect(Collectors.toList());
+    }
+
+    @Override
+    public Item findById(Long id, Integer cantidad) {
+        return new Item(productoClienteRest.detalle(id),cantidad) ;
+    }
+}
