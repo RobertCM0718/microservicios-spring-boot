430470830e7b3e3550177d99f59c813a7570f5ba
RobertCM0718 <robertcm0718@gmail.com>
2023-06-24 18:23:59 -0600

    Se configura uri remota. Se retira content-type text/plain Se crea archivo bootstrap.properties en items. Se agrega actuator y service obtener config.

--- microservicio-item/src/main/java/com/quetzalcode/items/controller/ItemController.java
+++ microservicio-item/src/main/java/com/quetzalcode/items/controller/ItemController.java
@@ -5,20 +5,34 @@ import com.quetzalcode.items.dto.Producto;
 import com.quetzalcode.items.service.IItemService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+import org.apache.http.protocol.HTTP;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
+@RefreshScope
 @RestController
 public class ItemController {
-
     private final Logger LOG = LoggerFactory.getLogger(ItemController.class);
+
+    @Autowired
+    private Environment environment;
+
+    @Value("${configuracion.texto}")
+    private String texto;
 
     @Autowired
     private CircuitBreakerFactory circuitBreakerFactory;
@@ -80,5 +94,20 @@ public class ItemController {
         producto.setPrecio(500.00);
         item.setProducto(producto);
         return CompletableFuture.supplyAsync(() -> item);
+    }
+
+    @GetMapping("/obtener-config")
+    public ResponseEntity<?> obtenerConfig(@Value("${server.port}")String puerto){
+        Map<String, String> json = new HashMap<>();
+        json.put("texto",texto);
+        json.put("puerto",puerto);
+
+        if (environment.getActiveProfiles().length > 0 && environment.getActiveProfiles()[0].equals("dev")){
+            json.put("autor.nombre", environment.getProperty("configuracion.autor.nombre"));
+            json.put("autor.email", environment.getProperty("configuracion.autor.email"));
+        }
+
+        LOG.info(texto);
+        return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
     }
 }
