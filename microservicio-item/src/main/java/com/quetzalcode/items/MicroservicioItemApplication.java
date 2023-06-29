3a0bf125fe2ffbfbd069b83113d79118c199030a
RobertCM0718 <robertcm0718@gmail.com>
2023-06-28 20:23:29 -0600

    Se crea proyecto usuarios.

--- microservicio-item/src/main/java/com/quetzalcode/items/MicroservicioItemApplication.java
+++ microservicio-item/src/main/java/com/quetzalcode/items/MicroservicioItemApplication.java
@@ -1,12 +1,15 @@
 package com.quetzalcode.items;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @EnableFeignClients
 @ImportAutoConfiguration({FeignAutoConfiguration.class})
 @SpringBootApplication
--- microservicio-productos/src/main/java/com/quetzalcode/productos/entity/Producto.java
+++ microservicio-productos/src/main/java/com/quetzalcode/productos/entity/Producto.java
@@ -1,6 +1,16 @@
 package com.quetzalcode.productos.entity;
 
-import jakarta.persistence.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.persistence.Transient;
+
 
 import java.io.Serializable;
 import java.util.Date;
