0a8f36562e017f9e7233ad72c0acf805332faad5
RobertCM0718 <robertcm0718@gmail.com>
2023-07-07 19:23:01 -0600

    Se comenta dependencia spring data rest.

--- microservicio-usuarios/src/main/java/com/quetzalcode/usuarios/config/RepositoryConfig.java
+++ microservicio-usuarios/src/main/java/com/quetzalcode/usuarios/config/RepositoryConfig.java
@@ -5,16 +5,15 @@ import com.quetzalcode.commons.usuarios.entity.Role;
 import com.quetzalcode.commons.usuarios.entity.Usuario;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
-@Configuration
+//@Configuration
 public class RepositoryConfig {
 
-    @Bean
-    public RepositoryRestConfigurer repositoryRestConfigurer() {
-        return RepositoryRestConfigurer.withConfig(config -> {
-            config.exposeIdsFor(Usuario.class, Role.class);
-        });
-    }
+//    @Bean
+//    public RepositoryRestConfigurer repositoryRestConfigurer() {
+//        return RepositoryRestConfigurer.withConfig(config -> {
+//            config.exposeIdsFor(Usuario.class, Role.class);
+//        });
+//    }
 
 }
--- microservicio-usuarios/src/main/java/com/quetzalcode/usuarios/repository/UsuarioRepository.java
+++ microservicio-usuarios/src/main/java/com/quetzalcode/usuarios/repository/UsuarioRepository.java
@@ -5,7 +5,6 @@ import com.quetzalcode.commons.usuarios.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 //@RepositoryRestResource(path = "usuarios")
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
